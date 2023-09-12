730dd50d037565bde2b77f18b959f2098eed23e4
Bj Jackson <smthbrothrjdev@gmail.com>
2023-09-11 21:15:45 -0500

    add htmx and hyperscript

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  fetch: routeHandler
+})
+
+async function routeHandler(req:Request):Promise<Response>{
+  const url = new URL(req.url);
+
+  if (url.pathname ==='' || url.pathname=="/"){
+    return new Response(Bun.file("src/index.html"))
+  }
+   return new Response("NotFound",{status:404})
+}
