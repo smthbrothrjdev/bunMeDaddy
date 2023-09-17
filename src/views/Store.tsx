87b9cb6ccd4801ad20ae68d378690e7a458d8e80
Bj Jackson <smthbrothrjdev@gmail.com>
2023-09-16 22:02:30 -0500

    wip funcitonality of store page

--- src/views/Store.tsx
+++ src/views/Store.tsx
@@ -8,13 +8,16 @@ export default function Store({principle, store}:StoreProps){
     return (<>
             <h1 className="text-purple-700 text-8xl font-semibold mb-10">Buy Something!</h1>
 
-            <div className="flex justify-center	flex-wrap">
+            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 pb-10" >
                 {store.list().map(item=>{
                 console.log(item)
                 return <StoreItem item={item} key={item.id} />
             })}
-
         </div>
+            <p id="results">
+l
+            </p>
+            <button className="h-fit text-3xl text-white rounded-3xl w-1/3 h-1/6 border-2 bg-red-400 justify-center "> Submit</button>
             {principle =='parents' && <button >Add</button>}
 
         </>
@@ -28,7 +31,7 @@ type StoreItemProps = {
 function StoreItem({item}:StoreItemProps){
     return(
 
-        <div className='text-center justify-items-center my-3 mx-4 w-1/3 bg-purple-600  border-solid border border-white text-white rounded-3xl'>
+        <div hyperscript-header={item.id} _="on click toggle .selected" className='text-center justify-items-center my-3 mx-4 bg-purple-600 px-4 border-solid border border-white text-white rounded-3xl'>
 
             <div className="text-3xl">{item.item_name}</div>
             <hr className="flex w-1/2 m-auto"/>
